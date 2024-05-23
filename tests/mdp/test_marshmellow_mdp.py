ac63acaea1b44c9cca071a5ef379112ed6b188de
Tom Silver <tomssilver@gmail.com>
2024-05-23 16:09:14 -0400

    add DiscreteMDP with marshmellow example

---
+++ tests/mdp/test_marshmellow_mdp.py
@@ -0,0 +1,26 @@
+"""Tests for marshmellow_mdp.py."""
+
+import numpy as np
+
+from mlrp_course.mdp.marshmellow_mdp import MarshmallowMDP
+
+
+def test_marshmellow_mdp():
+    """Tests for marshmellow_mdp.py."""
+    mdp = MarshmallowMDP()
+    assert (1, True) in mdp.state_space
+    assert "eat" in mdp.action_space
+    assert not mdp.state_is_terminal((1, True))
+    dist = mdp.get_transition_distribution((1, True), "eat")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist[(0, False)], 1.0)
+    dist = mdp.get_transition_distribution((1, False), "eat")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist[(1, False)], 0.75)
+    assert np.isclose(dist[(2, False)], 0.25)
+    reward = mdp.get_reward((1, True), "eat", (0, False))
+    assert np.isclose(reward, 0.0)
+    reward = mdp.get_reward((1, True), "wait", (1, True))
+    assert np.isclose(reward, -1.0)
+    reward = mdp.get_reward((1, True), "wait", (2, True))
+    assert np.isclose(reward, -4.0)
