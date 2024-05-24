73bc39637113ead127e59a0260346d8d73ca4d28
Tom Silver <tomssilver@gmail.com>
2024-05-23 21:10:52 -0400

    add test

---
+++ tests/algorithms/test_finite_horizon_dp.py
@@ -0,0 +1,15 @@
+"""Tests for finite_horizon_dp.py."""
+
+import numpy as np
+
+from mlrp_course.algorithms.finite_horizon_dp import finite_horizon_dp
+from mlrp_course.mdp.marshmellow_mdp import MarshmallowMDP
+
+
+def test_finite_horizon_dp():
+    """Tests for finite_horizon_dp.py."""
+    mdp = MarshmallowMDP()
+    V = finite_horizon_dp(mdp)
+    assert all(np.isclose(v, 0.0) for v in V[4].values())
+    assert V[0][(0, True)] > V[0][(1, True)]
+    assert V[0][(0, True)] > V[0][(0, False)]
