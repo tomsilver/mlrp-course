583a5ef9e8421618b45fd7dd3f0c7978a9eaeebd
Tom Silver <tomssilver@gmail.com>
2024-05-24 12:03:07 -0400

    adding large chase mdp

--- src/mlrp_course/mdp/chase_mdp.py
+++ src/mlrp_course/mdp/chase_mdp.py
@@ -9,6 +9,7 @@ from typing import (
     ClassVar,
     DefaultDict,
     Dict,
+    List,
     Optional,
     Set,
     Tuple,
@@ -25,6 +26,7 @@ from mlrp_course.utils import load_image_asset
 
 # Define the state and action types.
 ChaseAction: TypeAlias = str
+BunnyPosition: TypeAlias = Optional[Tuple[int, int]]
 
 
 @dataclass(frozen=True)
@@ -32,7 +34,7 @@ class ChaseState:
     """The state consists of the robot position and the bunny positions."""
 
     robot_pos: Tuple[int, int]
-    bunny_positions: Tuple[Optional[Tuple[int, int]], ...]  # location or gone
+    bunny_positions: Tuple[BunnyPosition, ...]  # location or gone
 
     def __lt__(self, other: Any) -> bool:
         assert isinstance(other, ChaseState)
@@ -86,14 +88,9 @@ class ChaseMDP(DiscreteMDP[ChaseState, ChaseAction]):
             "right": (0, 1),
         }[action]
 
-    def get_transition_distribution(
+    def _get_next_robot_pos(
         self, state: ChaseState, action: ChaseAction
-    ) -> Dict[ChaseState, float]:
-        # Discrete distributions, represented with a dict
-        # mapping next states to probs.
-        next_state_dist: Dict[ChaseState, float] = {}
-
-        # Get next agent state.
+    ) -> Tuple[int, int]:
         row, col = state.robot_pos
         dr, dc = self._action_to_delta(action)
         r, c = row + dr, col + dc
@@ -102,12 +99,15 @@ class ChaseMDP(DiscreteMDP[ChaseState, ChaseAction]):
             r, c = row, col
         elif self._obstacles[r, c]:
             r, c = row, col
-        next_robot_pos = (r, c)
+        return (r, c)
 
+    def _get_next_bunny_distributions(
+        self, state: ChaseState, next_robot_pos: Tuple[int, int]
+    ) -> List[Dict[BunnyPosition, float]]:
         # Get next bunny state distributions.
-        next_bunny_pos_dists = []
+        next_bunny_pos_dists: List[Dict[BunnyPosition, float]] = []
         for bunny_pos in state.bunny_positions:
-            dist: DefaultDict[Optional[Tuple[int, int]], float] = defaultdict(float)
+            dist: DefaultDict[BunnyPosition, float] = defaultdict(float)
             # If the bunny is already gone, it will definitely be gone.
             if bunny_pos is None:
                 dist[None] = 1.0
@@ -132,6 +132,16 @@ class ChaseMDP(DiscreteMDP[ChaseState, ChaseAction]):
                     else:
                         dist[next_bunny_pos] += 0.5 * 0.25
             next_bunny_pos_dists.append(dist)
+        return next_bunny_pos_dists
+
+    def get_transition_distribution(
+        self, state: ChaseState, action: ChaseAction
+    ) -> Dict[ChaseState, float]:
+        # Discrete distributions, represented with a dict
+        # mapping next states to probs.
+        next_state_dist: Dict[ChaseState, float] = {}
+        next_robot_pos = self._get_next_robot_pos(state, action)
+        next_bunny_pos_dists = self._get_next_bunny_distributions(state, next_robot_pos)
 
         # Combine bunny distributions together.
         for outcome in itertools.product(*[d.items() for d in next_bunny_pos_dists]):
@@ -271,3 +281,32 @@ class ChaseWithLargeRoomsMDP(ChaseMDP):
         ],
         dtype=np.bool_,
     )
+
+
+class LargeChaseMDP(ChaseMDP):
+    """A variation with arbitrarily many bunnies in a large free space."""
+
+    _obstacles: ClassVar[NDArray[np.bool_]] = np.zeros((32, 32), dtype=np.bool_)
+
+    @property
+    def state_space(self) -> Set[ChaseState]:
+        raise NotImplementedError("State space too large to enumerate.")
+
+    def get_transition_distribution(
+        self, state: ChaseState, action: ChaseAction
+    ) -> Dict[ChaseState, float]:
+        raise NotImplementedError("Transition distribution too large.")
+
+    def sample_next_state(
+        self, state: ChaseState, action: ChaseAction, rng: np.random.Generator
+    ) -> ChaseState:
+        next_robot_pos = self._get_next_robot_pos(state, action)
+        next_bunny_pos_dists = self._get_next_bunny_distributions(state, next_robot_pos)
+        sampled_bunny_positions = []
+        for dist in next_bunny_pos_dists:
+            options, probs = zip(*dist.items())
+            idxs = len(options)
+            idx = rng.choice(idxs, p=probs)
+            pos = options[idx]
+            sampled_bunny_positions.append(pos)
+        return ChaseState(next_robot_pos, tuple(sampled_bunny_positions))
