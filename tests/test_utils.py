78c01b5ccd354428f3651eb6b13da7c6f251e5ee
Tom Silver <tomsilver@users.noreply.github.com>
2024-05-25 10:35:41 -0400

    add gym wrapper for discrete MDPs  (#12)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,28 @@
 """Tests for utils.py."""
+
+from typing import Optional
+
+from mlrp_course.mdp.discrete_mdp import DiscreteState
+from mlrp_course.mdp.marshmellow_mdp import MarshmallowMDP
+from mlrp_course.utils import DiscreteMDPGymEnv
+
+
+def test_discrete_mdp_gym_env():
+    """Tests for DiscreteMDPGymEnv()."""
+    mdp = MarshmallowMDP()
+
+    def _sample_initial_state(_: Optional[int]) -> DiscreteState:
+        return (0, True)
+
+    env = DiscreteMDPGymEnv(mdp, _sample_initial_state)
+
+    obs, _ = env.reset(seed=123)
+    assert obs == (0, True)
+
+    for _ in range(3):
+        obs, _, terminated, truncated, _ = env.step("wait")
+        assert obs in mdp.state_space
+        assert not terminated
+        assert not truncated
+    _, _, _, truncated, _ = env.step("wait")
+    assert truncated
