b252f86c2bce28f48d22663d4e616e5081d3ef50
Tom Silver <tomsilver@users.noreply.github.com>
2024-05-28 18:03:38 -0400

    more example POMDPs (#21)

---
+++ src/mlrp_course/pomdp/envs/search_and_rescue_pomdp.py
@@ -0,0 +1,191 @@
+"""Search and Rescue POMDP."""
+
+from collections import defaultdict
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Dict, List, Set, Tuple
+
+import numpy as np
+
+from mlrp_course.pomdp.discrete_pomdp import DiscretePOMDP
+from mlrp_course.structs import Hyperparameters, Image
+
+
+@dataclass(frozen=True, eq=True, order=True)
+class SearchAndRescueObs:
+    """An observation in the SearchAndRescuePOMDP."""
+
+    robot_loc: Tuple[int, int]
+    scan_response: str | None = None  # got-response, got-no-response
+
+
+@dataclass(frozen=True, eq=True, order=True)
+class SearchAndRescueState:
+    """A state in the SearchAndRescuePOMDP."""
+
+    robot_loc: Tuple[int, int]
+    person_loc: Tuple[int, int]
+
+
+@dataclass(frozen=True, eq=True, order=True)
+class SearchAndRescueAction:
+    """An action in the SearchAndRescuePOMDP."""
+
+    type: str  # move or scan
+    direction: Tuple[int, int]  # up, down, left, right
+
+
+@dataclass(frozen=True)
+class SearchAndRescuePOMDPHyperparameters(Hyperparameters):
+    """Hyperparameters for the SearchAndRescuePOMDP."""
+
+    scan_noise_probability: float = 0.1
+    move_noise_probability: float = 0.1
+    living_reward: float = -1.0
+    rescue_reward: float = 100.0
+    fire_reward: float = -100.0
+
+
+class SearchAndRescuePOMDP(
+    DiscretePOMDP[SearchAndRescueObs, SearchAndRescueState, SearchAndRescueAction]
+):
+    """Search and Rescue POMDP."""
+
+    # Short-hand for grid cells.
+    E, W, F, H = _EMPTY, _WALL, _FIRE, _HIDDEN = range(4)
+
+    # Default medium-sized grid.
+    _grid = np.array(
+        [
+            [W, W, H, E, E, E, E],
+            [W, F, E, W, E, W, E],
+            [F, F, E, E, E, E, H],
+            [E, F, E, E, E, F, E],
+            [H, E, E, E, E, F, F],
+            [E, W, E, W, E, F, W],
+            [E, E, E, E, H, W, W],
+        ]
+    )
+
+    def __init__(
+        self, config: SearchAndRescuePOMDPHyperparameters | None = None
+    ) -> None:
+        self._config = config or SearchAndRescuePOMDPHyperparameters()
+
+    @cached_property
+    def _possible_robot_locs(self) -> List[Tuple[int, int]]:
+        mask = self._grid == self._EMPTY
+        mask |= self._grid == self._HIDDEN
+        mask |= self._grid == self._FIRE
+        return [tuple(loc) for loc in np.argwhere(mask)]
+
+    @cached_property
+    def _possible_person_locs(self) -> List[Tuple[int, int]]:
+        mask = self._grid == self._HIDDEN
+        return [tuple(loc) for loc in np.argwhere(mask)]
+
+    @property
+    def observation_space(self) -> Set[SearchAndRescueObs]:
+        observations: Set[SearchAndRescueObs] = set()
+        for scan_response in ["got-response", "got-no-response", None]:
+            for robot_loc in self._possible_robot_locs:
+                observations.add(SearchAndRescueObs(robot_loc, scan_response))
+        return observations
+
+    @property
+    def state_space(self) -> Set[SearchAndRescueState]:
+        states: Set[SearchAndRescueState] = set()
+        for person_loc in self._possible_person_locs:
+            for robot_loc in self._possible_robot_locs:
+                states.add(SearchAndRescueState(robot_loc, person_loc))
+        return states
+
+    @property
+    def action_space(self) -> Set[SearchAndRescueAction]:
+        actions: Set[SearchAndRescueAction] = set()
+        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            for t in ["move", "scan"]:
+                actions.add(SearchAndRescueAction(t, d))
+        return actions
+
+    def get_observation_distribution(
+        self, next_state: SearchAndRescueState, action: SearchAndRescueAction
+    ) -> Dict[SearchAndRescueObs, float]:
+        robot_loc = next_state.robot_loc
+        if action.type == "move":
+            return {SearchAndRescueObs(robot_loc): 1.0}
+        assert action.type == "scan"
+        # If the action was a scan, get a noisy response.
+        # First figure out the "correct" directions to scan.
+        person_loc = next_state.person_loc
+        correct_scans: List[Tuple[int, int]] = []
+        dr, dc = np.subtract(person_loc, robot_loc)
+        if dr < 0:
+            correct_scans.append((-1, 0))
+        elif dr > 0:
+            correct_scans.append((1, 0))
+        if dc < 0:
+            correct_scans.append((0, -1))
+        elif dc > 0:
+            correct_scans.append((0, 1))
+        assert len(correct_scans) <= 2
+        if action.direction in correct_scans:
+            correct_response = "got-response"
+            incorrect_response = "got-no-response"
+        else:
+            correct_response = "got-no-response"
+            incorrect_response = "got-response"
+        # Return a noisy response.
+        noise_prob = self._config.scan_noise_probability
+        dist = {
+            SearchAndRescueObs(robot_loc, correct_response): 1.0 - noise_prob,
+            SearchAndRescueObs(robot_loc, incorrect_response): noise_prob,
+        }
+        return dist
+
+    def state_is_terminal(self, state: SearchAndRescueState) -> bool:
+        return state.robot_loc == state.person_loc
+
+    def get_reward(
+        self,
+        state: SearchAndRescueState,
+        action: SearchAndRescueAction,
+        next_state: SearchAndRescueState,
+    ) -> float:
+        reward = self._config.living_reward
+        if self.state_is_terminal(next_state):
+            reward += self._config.rescue_reward
+        # If in fire, big negative reward.
+        next_r, next_c = next_state.robot_loc
+        if self._grid[next_r, next_c] == self._FIRE:
+            reward += self._config.fire_reward
+        return reward
+
+    def get_transition_distribution(
+        self, state: SearchAndRescueState, action: SearchAndRescueAction
+    ) -> Dict[SearchAndRescueState, float]:
+        # Scanning does not change the state.
+        if action.type == "scan":
+            return {state: 1.0}
+        assert action.type == "move"
+        robot_r, robot_c = state.robot_loc
+        person_loc = state.person_loc
+        # Moving is deterministically null if we're in a fire.
+        if self._grid[robot_r, robot_c] == self._FIRE:
+            return {state: 1.0}
+        # Moving is stochastic otherwise.
+        dist: Dict[SearchAndRescueState, float] = defaultdict(float)
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if (robot_r + dr, robot_c + dc) in self._possible_robot_locs:
+                next_robot_loc = (robot_r + dr, robot_c + dc)
+            else:
+                next_robot_loc = (robot_r, robot_c)
+            next_state = SearchAndRescueState(next_robot_loc, person_loc)
+            if (dr, dc) == action.direction:
+                dist[next_state] += 1.0 - self._config.move_noise_probability
+            else:
+                dist[next_state] += self._config.move_noise_probability / 3.0
+        return dist
+
+    def render_state(self, state: SearchAndRescueState) -> Image:
+        raise NotImplementedError("Rendering not implemented for POMDP.")
---
+++ tests/pomdp/envs/test_tiger_pomdp.py
@@ -0,0 +1,32 @@
+"""Tests for tiger_pomdp.py."""
+
+import numpy as np
+
+from mlrp_course.pomdp.envs.tiger_pomdp import TigerPOMDP
+
+
+def test_tiger_pomdp():
+    """Tests for tiger_pomdp.py."""
+    pomdp = TigerPOMDP()
+    assert "hear-left" in pomdp.observation_space
+    assert "tiger-left" in pomdp.state_space
+    assert "listen" in pomdp.action_space
+    dist = pomdp.get_observation_distribution("tiger-left", "listen")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist["hear-left"], 0.85)
+    dist = pomdp.get_observation_distribution("tiger-right", "listen")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist["hear-left"], 0.15)
+    dist = pomdp.get_observation_distribution("tiger-left", "open-left")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist["none"], 1.0)
+    assert not pomdp.state_is_terminal("tiger-left")
+    dist = pomdp.get_transition_distribution("tiger-left", "listen")
+    assert np.isclose(sum(dist.values()), 1.0)
+    assert np.isclose(dist["tiger-left"], 1.0)
+    reward = pomdp.get_reward("tiger-left", "listen", "tiger-left")
+    assert np.isclose(reward, -1)
+    reward = pomdp.get_reward("tiger-left", "open-left", "tiger-left")
+    assert np.isclose(reward, -100)
+    reward = pomdp.get_reward("tiger-left", "open-right", "tiger-left")
+    assert np.isclose(reward, 10)
