f8dce89f41993011952c87b3992190ea1f139a02
Tom Silver <tomssilver@gmail.com>
2024-06-05 15:12:52 -0400

    wip

--- src/mlrp_course/classical/envs/classical_problem.py
+++ src/mlrp_course/classical/envs/classical_problem.py
@@ -1,8 +1,7 @@
-"""Classical planning domains and problems."""
+"""Classical planning problems."""
 
 import abc
-from dataclasses import dataclass
-from typing import Callable, Generic, Set, TypeAlias, TypeVar
+from typing import Generic, Set, TypeAlias, TypeVar
 
 from mlrp_course.structs import HashableComparable, Image
 
@@ -13,8 +12,8 @@ _S = TypeVar("_S", bound=DiscreteState)
 _A = TypeVar("_A", bound=DiscreteAction)
 
 
-class ClassicalPlanningDomain(Generic[_S, _A]):
-    """A classical planning domain."""
+class ClassicalPlanningProblem(Generic[_S, _A]):
+    """A classical planning problem."""
 
     @property
     @abc.abstractmethod
@@ -25,6 +24,11 @@ class ClassicalPlanningDomain(Generic[_S, _A]):
     @abc.abstractmethod
     def action_space(self) -> Set[_A]:
         """Representation of the action set."""
+
+    @property
+    @abc.abstractmethod
+    def initial_state(self) -> _S:
+        """The initial state."""
 
     @abc.abstractmethod
     def initiable(self, state: _S, action: _A) -> bool:
@@ -39,14 +43,9 @@ class ClassicalPlanningDomain(Generic[_S, _A]):
         """The transition function."""
 
     @abc.abstractmethod
-    def render_state(self, state: _S, goal: Callable[[_S], bool]) -> Image:
+    def check_goal(self, _S) -> bool:
+        """The goal function."""
+
+    @abc.abstractmethod
+    def render_state(self, state: _S) -> Image:
         """Optional rendering function for visualizations."""
-
-
-@dataclass(frozen=True)
-class ClassicalPlanningProblem(Generic[_S, _A]):
-    """A classical planning problem."""
-
-    domain: ClassicalPlanningDomain[_S, _A]
-    initial_state: _S
-    goal: Callable[[_S], bool]
---
+++ src/mlrp_course/classical/envs/pddl_problem.py
@@ -0,0 +1,65 @@
+"""A classical planning problem defined with PDDL."""
+
+from dataclasses import dataclass
+from typing import Callable, FrozenSet, Set, TypeAlias
+
+from relational_structs import GroundAtom, GroundOperator, PDDLDomain, PDDLProblem
+from relational_structs.utils import all_ground_operators
+
+from mlrp_course.classical.envs.classical_problem import ClassicalPlanningProblem
+from mlrp_course.structs import Image
+
+PDDLState: TypeAlias = FrozenSet[GroundAtom]
+PDDLAction: TypeAlias = GroundOperator
+
+
+@dataclass(frozen=True)
+class PDDLGoal:
+    """A goal for a PDDL planning problem."""
+
+    goal_atoms: FrozenSet[GroundAtom]
+
+    def __call__(self, state: PDDLState) -> bool:
+        return self.goal_atoms.issubset(state)
+
+
+class PDDLPlanningProblem(ClassicalPlanningProblem[PDDLState, PDDLAction]):
+    """A classical planning problem defined with PDDL."""
+
+    def __init__(
+        self,
+        pddl_domain_str: str,
+        pddl_problem_str: str,
+        render_fn: Callable[[PDDLState, PDDLGoal], Image] | None = None,
+    ) -> None:
+        self._pddl_domain = PDDLDomain.parse(pddl_domain_str)
+        self._pddl_problem = PDDLProblem.parse(pddl_problem_str, self._pddl_domain)
+        self._render_fn = render_fn
+        self._all_ground_operators = all_ground_operators(
+            self._pddl_domain.operators, self._pddl_problem.objects
+        )
+
+    @property
+    def state_space(self) -> Set[PDDLState]:
+        raise NotImplementedError("PDDL state spaces cannot be enumerated")
+
+    @property
+    def action_space(self) -> Set[PDDLAction]:
+        return self._all_ground_operators
+
+    def initiable(self, state: PDDLState, action: PDDLAction) -> bool:
+        return action.preconditions.issubset(state)
+
+    def get_cost(
+        self, state: PDDLState, action: PDDLAction, next_state: PDDLState
+    ) -> float:
+        return 1.0
+
+    def get_next_state(self, state: PDDLState, action: PDDLAction) -> PDDLState:
+        return (state - action.delete_effects) | action.add_effects
+
+    def render_state(self, state: PDDLState) -> Image:
+        if self._render_fn is None:
+            raise NotImplementedError
+        goal = PDDLGoal(self._pddl_problem.goal)
+        return self._render_fn(state, goal)
