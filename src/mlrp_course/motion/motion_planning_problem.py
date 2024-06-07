ea8d7e9bd936d8b3f4627bab62027daa7c1105ff
Tom Silver <tomssilver@gmail.com>
2024-06-07 11:19:53 -0400

    wip

---
+++ src/mlrp_course/motion/motion_planning_problem.py
@@ -0,0 +1,31 @@
+"""A general motion planning problem."""
+
+import abc
+from typing import Generic, TypeVar
+
+import gymnasium as gym
+
+_Configuration = TypeVar("_Configuration")  # robot configuration
+
+
+class MotionPlanningProblem(Generic[_Configuration]):
+    """A general motion planning problem."""
+
+    @property
+    @abc.abstractmethod
+    def configuration_space(self) -> gym.Space[_Configuration]:
+        """A configuration space that can be sampled."""
+
+    @property
+    @abc.abstractmethod
+    def initial_configuration(self) -> _Configuration:
+        """The initial robot configuration."""
+
+    @property
+    @abc.abstractmethod
+    def goal_configuration(self) -> _Configuration:
+        """The goal robot configuration."""
+
+    @abc.abstractmethod
+    def has_collision(self, configuration: _Configuration) -> bool:
+        """Collision checking."""
