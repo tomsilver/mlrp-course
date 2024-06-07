6038f8fb738453bd2c9459f9030872d0f01b86d2
Tom Silver <tomssilver@gmail.com>
2024-06-07 14:23:39 -0400

    wip

--- src/mlrp_course/motion/motion_planning_problem.py
+++ src/mlrp_course/motion/motion_planning_problem.py
@@ -7,31 +7,31 @@ import gymnasium as gym
 
 from mlrp_course.structs import Image
 
-_Configuration = TypeVar("_Configuration")  # robot configuration
+RobotConf = TypeVar("RobotConf")  # robot configuration
 
 
-class MotionPlanningProblem(Generic[_Configuration]):
+class MotionPlanningProblem(Generic[RobotConf]):
     """A general motion planning problem."""
 
     @property
     @abc.abstractmethod
-    def configuration_space(self) -> gym.Space[_Configuration]:
+    def configuration_space(self) -> gym.Space[RobotConf]:
         """A configuration space that can be sampled."""
 
     @property
     @abc.abstractmethod
-    def initial_configuration(self) -> _Configuration:
+    def initial_configuration(self) -> RobotConf:
         """The initial robot configuration."""
 
     @property
     @abc.abstractmethod
-    def goal_configuration(self) -> _Configuration:
+    def goal_configuration(self) -> RobotConf:
         """The goal robot configuration."""
 
     @abc.abstractmethod
-    def has_collision(self, configuration: _Configuration) -> bool:
+    def has_collision(self, configuration: RobotConf) -> bool:
         """Collision checking."""
 
     @abc.abstractmethod
-    def render(self, configuration: _Configuration | None = None) -> Image:
+    def render(self, configuration: RobotConf | None = None) -> Image:
         """Optional rendering function."""
